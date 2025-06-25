54c2d5374fe5cf780e1b7ec3a2fc48354d4878f0
delloydb <lloydbrownjnr@gmail.com>
2025-06-25 17:02:53 +0300

    Create categoryController.js

---
+++ server/controller/categoryController.js
@@ -0,0 +1,14 @@
+
+// server/controllers/categoryController.js
+import Category from '../models/Category.js';
+
+export const getCategories = async (req, res) => {
+  const categories = await Category.find();
+  res.json(categories);
+};
+
+export const createCategory = async (req, res) => {
+  const category = new Category(req.body);
+  await category.save();
+  res.status(201).json(category);
+};
