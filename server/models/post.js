a35e957e28fde58d9522eaf00639431b0ac0003c
delloydb <lloydbrownjnr@gmail.com>
2025-06-25 16:59:58 +0300

    Create post.js

---
+++ server/models/post.js
@@ -0,0 +1,12 @@
+// server/models/Post.js
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+  image: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model('Post', postSchema);
